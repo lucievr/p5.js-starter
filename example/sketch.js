c05bd4bdef53acd5c3f5dd68085bcb62ccfbd5c8
lucievr <lucy.vrsovska@gmail.com>
2019-11-03 23:51:29 +0100

    Create canvas and draw function to create shapes on mouseover

--- example/sketch.js
+++ example/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  // put setup code here
+  createCanvas(640, 480);
+  background(0,200,0);
 }
 
 function draw() {
-  // put drawing code here
+  if (mouseIsPressed) {
+    fill(0);
+    ellipse(mouseX, mouseY, 80, 80);
+  } else {
+    fill(255);
+    rect(mouseX, mouseY, 80, 80);
+  }
 }
+
+// 4 arguments (x location, y location, width, height)
+// x, y (0,0) is the top left corner of the canvs
+
+// rect, ellipse, line, arc, point...
+
+// stroke() sets the outline/border color
+// fill() fills the inside of the shape with color
